4e4e77fb7e3d9467445c193184382cac3dedf4f2
Frida Thorkildsen Solhaug <frida.solhaug@fremtind.no>
2020-12-26 15:50:48 +0100

    Backupplan

--- src/Pages/Funfact.tsx
+++ src/Pages/Funfact.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "@fremtind/jkl-core";
+import "./Funfact.scss";
 
 export function Funfact() {
   return (
-    <div>
-      <h1>Spill</h1>
-      <form>Tessst</form>
+    <div className="funfact">
+      <h1>Svar ærlig på spørsmålene</h1>
+      <Link href="https://docs.google.com/forms/d/e/1FAIpQLScw3VcI3-PhzHktHaYpTiVuQCXEKS0kW58d2BT8ayuoWjqytA/viewform">
+        Trykk her
+      </Link>
     </div>
   );
 }
